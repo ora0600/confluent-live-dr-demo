735f89f85064fb88159fa15d84a92b4c15c0f700
ora0600 <github@carsten-muetzlitz.de>
2024-03-12 14:54:50 +0100

    describe mirror topics

--- Part3/active-active/00_list_all_topics.sh
+++ Part3/active-active/00_list_all_topics.sh
@@ -4,3 +4,8 @@ echo "List all topics on secondary cluster: "
 kafka-topics --list --bootstrap-server $destination_bootstrap --command-config ../kafkatools_consumer_secondary.properties
 echo "List all topics on primary cluster: "
 kafka-topics  --list --bootstrap-server $source_bootstrap --command-config ../kafkatools_consumer_primary.properties
+
+echo "List all mirror topics on secondary cluster: "
+kafka-mirrors --describe --bootstrap-server $destination_bootstrap --command-config ../kafkatools_consumer_secondary.properties --link active-secondary-primary-cluster
+echo "List all mirror topics on primary cluster: "
+kafka-mirrors --describe --bootstrap-server $source_bootstrap --command-config ../kafkatools_consumer_primary.properties --link active-primary-secondary-cluster
--- Part3/active-passive/00_list_all_topics.sh
+++ Part3/active-passive/00_list_all_topics.sh
@@ -2,3 +2,6 @@
 source ./env-destination
 echo "List all topics on secondary cluster: "
 kafka-topics --bootstrap-server $destination_bootstrap --list --command-config ../kafkatools_consumer_secondary.properties
+
+echo "List all mirror topics on secondary cluster: "
+kafka-mirrors --describe --bootstrap-server $destination_bootstrap --command-config ../kafkatools_consumer_secondary.properties --link passive-primary-secondary-cluster
